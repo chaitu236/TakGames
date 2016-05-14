ede1c185116258a59ef9f1db8890fda0328ad92f
Chaitanya Vadrevu <chaitu236@tamu.edu>
2016-05-14 10:10:57 -0500

    after rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TakGames::Application.config.secret_token = 'd6ec707d4d75301d95e0032369adf9ec30f078895f5e8dbcefa196522518a2497f3c68ac0f064a38e909508aaf54324387e5a7a193ba82865fe6d618c7043fb8'
